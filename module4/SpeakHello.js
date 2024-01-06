426cc66a05e22584a2b921cf376cfffd0829102f
Gurnoor21 <154370668+Gurnoor21@users.noreply.github.com>
2024-01-06 23:49:28 +0530

    Add files via upload

---
+++ module4/SpeakHello.js
@@ -0,0 +1,32 @@
+
+
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
+
+
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
+
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
